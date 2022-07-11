11935629d3cfcbb19dd24e9eeafc24e4d01e03c0
saiyasone <saiyasone595@gmail.com>
2022-07-12 00:25:39 +0700

    finish stop watch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  isPlay = false;
+  ms: any = '0' + 0;
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+  hour: any = '0' + 0;
+  startTimer: any;
+
+  setPlay() {
+    this.isPlay = !this.isPlay;
+  }
+
+  reset() {
+    this.ms = '0' + 0;
+    this.sec = '0' + 0;
+    this.min = '0' + 0;
+    this.hour = '0' + 0;
+    this.stop();
+  }
+
+  play() {
+    if (!this.isPlay) {
+      this.isPlay = true;
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+        if (this.ms === 100) {
+          this.sec++;
+          this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+          this.ms = '0' + 0;
+        }
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+
+        if (this.min === 60) {
+          this.hour++;
+          this.hour = this.hour < 10 ? '0' + this.hour : this.hour;
+          this.min = '0' + 0;
+        }
+      }, 10);
+    } else {
+      this.stop();
+    }
+  }
+
+  stop() {
+    clearInterval(this.startTimer);
+    this.isPlay = false;
+  }
 }
